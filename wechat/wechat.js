e3d38a316021e14a35e8eddd5426de079fcdc42e
Jipeng Huang <1319639755@qq.com>
2017-11-30 13:41:50 +0800

    update code 上传临时素材

--- wechat/wechat.js
+++ wechat/wechat.js
@@ -2,11 +2,14 @@
 
 var Promise = require('bluebird');
 var request = Promise.promisify(require('request'))
-var util=require('./util');
+var util = require('./util');
+var fs = require('fs');
 var prefix = 'https://api.weixin.qq.com/cgi-bin/'; //前缀
 var api = {
-    accessToken: prefix + 'token?grant_type=client_credential'
+    accessToken: prefix + 'token?grant_type=client_credential',
+    upload: prefix + 'media/upload?', //上传零时素材
 }
+
 
 function Wechat(opts) {
     var that = this;
@@ -14,7 +17,7 @@ function Wechat(opts) {
     this.appSecret = opts.appSecret;
     this.getAccessToken = opts.getAccessToken; //获取票据的方法
     this.saveAccessToken = opts.saveAccessToken; //存储票据的方法
-    this.getAccessToken()
+    /*this.getAccessToken()
         .then(function(data) {
             try {
                 data = JSON.parse(data);
@@ -35,8 +38,44 @@ function Wechat(opts) {
             that.access_token = data.access_token; //挂载到实例上
             that.expires_in = data.expires_in; //过期字段
             that.saveAccessToken(data); //调用存储票据的方法
+        })*/
+    this.fetchAccessToken()
+}
+
+Wechat.prototype.fetchAccessToken = function(data) {
+    var that = this;
+    if(this.access_token && this.expires_in){
+        if(this.isValidAccessToken(this)){
+            return Promise.resolve(this);
+        }
+    }
+    this.getAccessToken()
+        .then(function(data) {
+            try {
+                data = JSON.parse(data);
+            } catch (e) {
+                //更新票据
+                return that.updateAccessToken()
+            }
+            if (that.isValidAccessToken(data)) { //拿到票据判断票据是否在有效期内
+                return Promise.resolve(data); //将data传下去
+                //return data;
+            } else {
+                //更新票据
+                return that.updateAccessToken()
+            }
+        })
+        .then(function(data) {
+            //拿到最终票据结果
+            that.access_token = data.access_token; //挂载到实例上
+            that.expires_in = data.expires_in; //过期字段
+            that.saveAccessToken(data); //调用存储票据的方法
+
+            return Promise.resolve(data)
         })
 }
+
+
 //判断是否过期
 Wechat.prototype.isValidAccessToken = function(data) {
     if (!data || !data.access_token || !data.expires_in) {
@@ -73,16 +112,53 @@ Wechat.prototype.updateAccessToken = function() {
 }
 
 Wechat.prototype.reply = function() {
-    var content=this.body;
-    var massage=this.weixin;
+    var content = this.body;
+    var massage = this.weixin;
     /*console.log(content)
     console.log(1111)*/
     //console.log(massage)
-    var xml=util.tpl(content,massage);
+    var xml = util.tpl(content, massage);
 
-    this.status=200;//设置回复状态为200
-    this.type='application/xml';//设置类型xml格式
-    this.body=xml;
+    this.status = 200; //设置回复状态为200
+    this.type = 'application/xml'; //设置类型xml格式
+    this.body = xml;
 }
 
-module.exports=Wechat;
+
+
+//上传方法
+Wechat.prototype.uploadMaterial = function(type, filepath) {
+    var that = this;
+    var form = {
+        media: fs.createReadStream(filepath)
+    }
+
+    var appID = this.appID;
+    var appSecret = this.appSecret;
+
+
+    return new Promise(function(resolve, reject) {
+        that
+            .fetchAccessToken()
+            .then(function(data) {
+                var url = api.upload + 'access_token=' + data.access_token + '&type=' + type;
+                //request 向某个服务器发请求
+                request({ method: 'POST', url: url, formData: form, json: true }).then(function(response) {
+                    var _data = response['body'];
+
+                    if (_data) {
+                        resolve(_data)
+                    } else {
+                        throw new Error('Upload material fails')
+                    }
+                })
+                .catch(function(err){
+                    reject(err)
+                })
+            })
+    })
+
+}
+
+module.exports = Wechat;
+
--- weixin.js
+++ weixin.js
@@ -1,29 +1,98 @@
 'use strict'
-exports.reply=function* (next){
-	var message=this.weixin;
 
-	if(message.MsgType ==='event'){
-		if(message.Event==='subscribe'){
-			if(message.EventKey){
-				console.log('扫码进来'+message.EventKey)
-			}
-			this.body='哈哈，你订阅了我的公众号'
-		}
-	}else if(message.Event==='unsubscribe'){
-		console.log('无情取关')
-	}else if(message.MsgType ==='text'){
-		console.log(message)
-		if(message.Content=='你好'){
-			this.body='你好'
-		}else if(message.Content=='李晗'){
-			this.body='我永远爱你亲爱的'
-		}else if(message.Content=='傻瓜'){
-			this.body='你才是傻瓜'
-		}else{
-			this.body='哈哈，你订阅了我的公众号'
-		}
-		//this.body='哈哈，你订阅了我的公众号'
-	}
+var config=require('./config');
+var Wechat=require('./wechat/wechat');
+var wechatApi=new Wechat(config.wechat);
 
-	yield next
+exports.reply = function*(next) {
+    var message = this.weixin;
+
+    if (message.MsgType === 'event') {
+        if (message.Event === 'subscribe') {
+            if (message.EventKey) {
+                console.log('扫码进来' + message.EventKey)
+            }
+            this.body = '哈哈，你订阅了我的公众号'
+        }
+        //取消关注
+        else if (message.Event === 'unsubscribe') {
+            console.log('无情取关');
+            this.body = ''
+        }
+        //地理位置
+        else if (message.Event === 'LOCATION') {
+            this.body = '您上报的位置是:' + message.Latitude + '/' + message.Longitude + '-' + message.Precision
+        }
+        //点击了菜单
+        else if (message.Event === 'CLICK') {
+            this.body = '您点击了菜单:' + message.EventKey
+        }
+        //扫描
+        else if (message.Event === 'SCAN') {
+            console.log('关注后二维码' + message.EventKey + ' ' + message.Ticket)
+            this.body = '看到你扫了一下哦';
+        }
+        //点击菜单中的链接
+        else if (message.Event === 'VIEW') {
+            this.body = '您点击了菜单中的链接' + message.EventKey;
+        }
+    } else if (message.MsgType === 'text') {
+        var content = message.Content;
+        var reply = '额,您说的' + content + ' 太复杂了'
+        console.log(message)
+
+        if (content == '1') {
+            reply = '天下第一吃大米';
+        } else if (content == '2') {
+            reply = '天下第二吃豆腐';
+        } else if (content == '3') {
+            reply = '天下第三吃仙丹';
+        }
+        //回复图文
+        else if (content == '4') {
+            reply = [{
+                title: '技术改变世界',
+                description: '只是个描述而已',
+                picUrl: 'http://static.samured.com/assets/images/video/cover/iQVBBrK_gEM.jpg',
+                url: 'http://www.huanghanlian.com/'
+            }, {
+                title: '游戏',
+                description: '好玩',
+                picUrl: 'http://static.samured.com/assets/images/video/cover/Dnnq9FDEubI.jpg',
+                url: 'https://github.com/huanghanzhilian'
+            }];
+        } else if (content == '5') {
+        	var data =yield wechatApi.uploadMaterial('image',__dirname +'/2.jpg');
+        	//console.log(data)
+            reply = {
+            	type:'image',
+            	mediaId:data.media_id
+            };
+        }
+        //视频上传
+        else if (content == '6') {
+        	var data =yield wechatApi.uploadMaterial('video',__dirname +'/6.mp4');
+        	//console.log(data)
+            reply = {
+            	type:'video',
+            	title:'回复视频',
+            	description: '描述',
+            	mediaId:data.media_id
+            };
+        }
+        //音乐
+        else if (content == '7') {
+        	var data =yield wechatApi.uploadMaterial('image',__dirname +'/2.jpg');
+            reply = {
+            	type:'music',
+            	title:'回复音乐内容',
+            	description: '放松一下',
+            	musicUrl:'http://96.f.1ting.com/5a1f988c/b755645dd872c6db0559cf9be27fe94d/zzzzzmp3/2017kNov/29X/29f_TizzyT/01.mp3',
+            	thumbMediaId:data.media_id
+            };
+        }
+        this.body = reply;
+    }
+
+    yield next
 };
